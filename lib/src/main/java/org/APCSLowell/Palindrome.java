be7fa0f635c348415a5cd214b24914670749200f
anharrison <reddyrocket16@gmail.com>
2026-02-05 18:03:02 +0000

    the

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,18 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        if (s == null) return true;
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+        String norm = cleaned.toString();
+        return norm.equals(new StringBuilder(norm).reverse().toString());
     }
     public String reverse(String s) {
-        String n = new String();
-        // your code here
-        return n;
+        if (s == null) return null;
+        return new StringBuilder(s).reverse().toString();
     }
 }
